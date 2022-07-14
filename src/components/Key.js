0cca66ca01d67d6b5afeabb1089dfcbaf80741a5
ParkerJSmith <parker.smith@shaw.ca>
2022-07-13 23:50:11 -0600

    works slightly better, still not good

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,21 +1,73 @@
 const Key = ({ tone }) => {
     var context = new AudioContext()
-    var o = context.createOscillator()
-    var  g = context.createGain()
-    o.connect(g)
-    g.connect(context.destination)
+    var o = context.createOscillator();
+    o.type = "sine";
+    var g = context.createGain();
+    var firstNote = true;
+    var frequency;
+    switch (tone) {
+        case "c":
+            frequency = 261.6;
+            break;
+        case "c#":
+            frequency = 277.2;
+            break;
+        case "d":
+            frequency = 293.7;
+            break;
+        case "d#":
+            frequency = 311.1;
+            break;
+        case "e":
+            frequency = 329.6;
+            break;
+        case "f":
+            frequency = 349.2;
+            break;
+        case "f#":
+            frequency = 370;
+            break;
+        case "g":
+            frequency = 392;
+            break;
+        case "g#":
+            frequency = 415.3;
+            break;
+        case "a":
+            frequency = 440;
+            break;
+        case "a#":
+            frequency = 466.2;
+            break;
+        case "b":
+            frequency = 493.9;
+            break;
+    }
+    o.connect(g);
+    g.connect(context.destination);
 
     const playNote = () => {
-        o.start(0)
+        //if (firstNote) {
+        //    o.start(0);
+        //    firstNote = false;
+        //} else {
+        //    g.gain.exponentialRampToValueAtTime(1.00000, context.currentTime + 0.1);
+        //}
+        o = context.createOscillator();
+        o.connect(g);
+        g.connect(context.destination);
+        o.frequency.value = frequency;
+        o.start();
     };
 
     const stopNote = () => {
-        g.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + 3);
+        //g.gain.exponentialRampToValueAtTime(0.000001, context.currentTime + 1.1);
+        o.stop();
     };
 
     return (
         <div className="key">
-            <div 
+            <div
                 className={(tone.includes('#') ? 'black-key' : 'white-key') + ' ' + tone.replace('#', '-sharp')}
                 onMouseDown={playNote}
                 onMouseUp={stopNote}
